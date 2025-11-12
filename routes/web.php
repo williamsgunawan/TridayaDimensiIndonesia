42209d11735ba5a3c1e11ddaa1c6edad1e381972
Williams Gunawan <z5325499@ad.unsw.edu.au>
2025-11-12 11:10:15 +0700

    remove other sites

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,3 @@ Route::get('/about', function () {
 Route::get('/our-clients', function () {
     return view('ourclients');
 });
-
-Route::get('/indodaya', function () {
-    return view('iat_home');
-});
-
-Route::get('/indodaya/services', function () {
-    return view('iat_services');
-});
-
-Route::get('/indodaya/career', function () {
-    return view('iat_career');
-});
-
-Route::get('/kotakhijauutama', function () {
-    return view('khu_home');
-});
-
-Route::get('/kotakhijauutama/program', function () {
-    return view('khu_program');
-});
-
-Route::get('/kotakhijauutama/services', function () {
-    return view('khu_services');
-});
